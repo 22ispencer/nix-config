1171705bb23a8a7fa02caad2b3dda265a063b61c
Isaac Spencer <22ispencer@gmail.com>
2025-04-04 22:54:01 -0500

    fixed backup directory

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -22,6 +22,23 @@
   :hook
   (prog-mode . display-line-numbers-mode))
 
+;; Setup backup directory
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+        (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
 (use-package org-auto-tangle
   :ensure t
   :defer t
@@ -62,6 +79,15 @@
   :ensure t
   :config
   (evil-collection-init))
+
+(use-package vimish-fold
+  :ensure t
+  :after evil)
+
+;(use-package evil-vimish-fold
+;  :ensure t
+;  :after vimish-fold
+;  :hook ((prog-mode conf-mode text-mode) . evil-vimish-foldn-mode))
 
 (use-package orderless
   :ensure t
