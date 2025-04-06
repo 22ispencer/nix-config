deb9ab8c742d5dbdae2e2fc645f75febb181861b
Isaac Spencer <22ispencer@gmail.com>
2025-04-05 21:22:41 -0500

    adjust emacs font height

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1,4 +1,4 @@
-(set-face-attribute 'default nil :font "Monaspace Argon" :height 180)
+(set-face-attribute 'default nil :font "Monaspace Argon" :height 160)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
