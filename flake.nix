241581d5a80668fcb0238c8d1595e1f0eeabee2d
Isaac Spencer <22ispencer@gmail.com>
2025-04-18 00:09:51 -0500

    firefox home manager

--- flake.nix
+++ flake.nix
@@ -19,6 +19,10 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
---
+++ home/firefox/default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  lib,
+  config,
+  options,
+  firefox-pkgs,
+}:
+let
+  mod = config.mods.firefox;
+in
+{
+  options.mods.firefox.enable = {
+    enable = lib.mkEnableOption "firefox";
+  };
+  config = lib.mkIf mod.enable {
+    programs.firefox = {
+      enable = true;
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+        }
+      };
+    };
+  };
+}
