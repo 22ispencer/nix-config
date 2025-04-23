fc8845cafbcefef15faadf2aa0bebf800b1d4935
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-04-23 21:31:21 +0000

    meson mode

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -186,6 +186,9 @@
   :custom
   (rust-mode-treesitter-derive t))
 
+(use-package meson-mode
+  :ensure t)
+
 (use-package nix-mode
   :ensure t
   :mode "\\.nix\\'")
@@ -224,15 +227,3 @@
   :general
   (:states 'normal
 	   "SPC v" 'vterm))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((org-confirm-babel-evaluate))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
