8c39ca04b77c35849896816ffd50aa074d1d23bc
Isaac Spencer <22ispencer@gmail.com>
2025-05-03 18:15:49 -0500

    added input remapper

--- zoro/configuration.nix
+++ zoro/configuration.nix
@@ -123,6 +123,10 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   };
 
+  services.input-remapper = {
+    enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
