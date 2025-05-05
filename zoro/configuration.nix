c487e8fffadc876805ce5ed6c40fa97eee76a2d0
Isaac Spencer <22ispencer@gmail.com>
2025-05-04 19:45:40 -0500

    disable xserver for plasma

--- zoro/configuration.nix
+++ zoro/configuration.nix
@@ -44,12 +44,9 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Desktop Environment
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
