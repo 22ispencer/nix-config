4345c642a8633a0468b8ee69ca160929bc3fad58
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-05-27 14:01:40 +0000

    tmux theme

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -21,9 +21,23 @@ in
         catppuccin
       ];
       extraConfig = ''
+        # setup leader
         unbind C-b
         set -g prefix C-Space
         bind C-Space send-prefix
+
+        ## catppuccin theme
+        # fix color
+        set -ag terminal-overrides ",$TERM:RGB"
+        # Make the status line pretty and add some modules
+        set -g status-right-length 100
+        set -g status-left-length 100
+        set -g status-left ""
+        set -g status-right "#{E:@catppuccin_status_application}"
+        set -agF status-right "#{E:@catppuccin_status_cpu}"
+        set -ag status-right "#{E:@catppuccin_status_session}"
+        set -ag status-right "#{E:@catppuccin_status_uptime}"
+        set -agF status-right "#{E:@catppuccin_status_battery}"
       '';
     };
   };
